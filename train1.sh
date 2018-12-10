b9262a8442f4b82dd5b49deec5745c1342e0b2a7
KotoriCANOE <sanma.kote@gmail.com>
2018-12-10 15:30:31 +0800

    Update

--- train1.sh
+++ train1.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=0
-python train.py "/my/Datasets/CelebA" --processes 2 --max-steps 256000 --random-seed 0 --device /gpu:0 --batch-size 16 --postfix 9
+python train.py "/my/Datasets/CelebA" --processes 2 --max-steps 256000 --random-seed 0 --device /gpu:0 --batch-size 16 --gan-type wgan --postfix 9
 
 exit
--- train2.sh
+++ train2.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=1
-python train.py "/my/Datasets/CelebA" --processes 2 --max-steps 256000 --random-seed 0 --device /gpu:1 --batch-size 16 --postfix 10
+python train.py "/my/Datasets/CelebA" --processes 2 --max-steps 512000 --random-seed 0 --device /gpu:1 --batch-size 16 --gan-type dragan --postfix 10
 
 exit
